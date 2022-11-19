6a75f05a4e537d0fce1b6e408190602a7bdc85ba
boris <boris.vladimirov13@gmail.com>
2022-11-19 19:02:16 +0600

    body_validation fixed

--- src/api/v1/auth/role.py
+++ src/api/v1/auth/role.py
@@ -29,8 +29,8 @@ def not_found(error):
     return make_response(jsonify({'error': 'Request data is invalid'}), HTTPStatus.FORBIDDEN)
 
 
-def body_validation(param: str):
-    if not request.json or param not in request.json:
+def body_validation(rrequest, param: str):
+    if not rrequest.json or param not in rrequest.json:
         abort(400)
 
 
@@ -39,7 +39,7 @@ def body_validation(param: str):
 @admin_required()
 @token_validation(request)
 def add_role():
-    body_validation(request.json.get('name'))
+    body_validation(request, request.json.get('name'))
     db = RoleService()
     return jsonify({'role': [db.add_role(name=request.json.get('name'),
                                          description=request.json.get('description')).as_dict]}), HTTPStatus.CREATED
@@ -50,7 +50,7 @@ def add_role():
 @admin_required()
 @token_validation(request)
 def delete_role():
-    body_validation(request.json.get('id'))
+    body_validation(request, request.json.get('id'))
     db = RoleService()
     return jsonify({'deleted role': [db.del_role(request.json.get('id')).as_dict]}), HTTPStatus.OK
 
@@ -78,7 +78,7 @@ def show_role(role_id):
 @admin_required()
 @token_validation(request)
 def update_role():
-    body_validation(request.json.get('id'))
+    body_validation(request, request.json.get('id'))
     db = RoleService()
     return jsonify({'role updated': [db.update_role(role_id=request.json.get('id'),
                                                     name=request.json.get('name'),
