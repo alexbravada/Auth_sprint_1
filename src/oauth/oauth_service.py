2a7efaa22f509844f6800cc2e554c94765735d8a
boris <boris.vladimirov13@gmail.com>
2022-12-17 16:23:18 +0600

    style fix

--- src/oauth/oauth_service.py
+++ src/oauth/oauth_service.py
@@ -1,15 +1,13 @@
-from abc import abstractmethod
 from abc import ABC
-from typing import Optional
+from abc import abstractmethod
+from urllib.parse import urlencode
+
 import requests
-import json
-import base64
 from flask import redirect
 
 from config.settings import Settings
 from db.user_service import UserService
 from utils.jaeger_wraps import trace
-from urllib.parse import urlencode
 
 SETTINGS = Settings()
 
@@ -20,7 +18,7 @@ class OAuthAbstract(ABC):
         pass
 
     @abstractmethod
-    def callback(self, auth_code: Optional):
+    def callback(self, auth_code: str, useragent: str):
         pass
 
 
@@ -31,19 +29,13 @@ class VKOAuth(OAuthAbstract):
         self.app_id = self.settings.get('id')
         self.secret = self.settings.get('secret')
         self.service_key = self.settings.get('service_key')
-
         self.redirect_uri = f'{SETTINGS.BASE_URL}/api/v1/oauth/callback/vk'
+        self.authorize_url = f'https://oauth.vk.com/authorize?client_id={self.app_id}' \
+                             f'&redirect_uri={self.redirect_uri}' \
+                             f'&display=page&scope=4194306&response_type=code&v=5.131'
 
     def authorize(self):
-        '''
-        display=page - open VK authorization in separate window
-        scope=4194306 - byte mask for 'friends' and 'email' permissions
-        response_type=code - required
-        '''
-        return redirect(
-            f'https://oauth.vk.com/authorize?client_id={self.app_id}&redirect_uri={self.redirect_uri}'
-            f'&display=page&scope=4194306&response_type=code&v=5.131',
-            code=302)
+        return redirect(self.authorize_url, code=302)
 
     @trace('VKOAuth.callback')
     def callback(self, auth_code, useragent) -> dict:
@@ -66,37 +58,35 @@ class YandexOAuth(OAuthAbstract):
         self.settings = SETTINGS.Yandex.dict()
         self.app_id = self.settings.get('id')
         self.secret = self.settings.get('secret')
-
         self.redirect_uri = f'{SETTINGS.BASE_URL}/api/v1/oauth/callback/yandex'
+        self.authorize_url = f'https://oauth.yandex.ru/authorize?client_id={self.app_id}' \
+                             f'&display=popup&response_type=code'
+
 
     def authorize(self):
-        '''
-        display=popup - open Yandex authorization in separate window
-        response_type=code - required
-        '''
-        return redirect(
-            f'https://oauth.yandex.ru/authorize?client_id={self.app_id}'  # &redirect_uri={self.redirect_uri}'
-            f'&display=popup&response_type=code',
-            code=302)
+        return redirect(self.authorize_url, code=302)
+
 
     @trace('YandexOAuth.callback')
     def callback(self, auth_code, useragent) -> dict:
-        response = requests.post(
-            'https://oauth.yandex.ru/token',
-            urlencode({'grant_type': 'authorization_code',
-                       'client_id': self.app_id,
-                       'client_secret': self.secret,
-                       'code': auth_code})).json()
+        @trace('YandexOAuth.token_swap')
+        def token_swap() -> dict:
+            return requests.post(url='https://oauth.yandex.ru/token',
+                                 data=urlencode({'grant_type': 'authorization_code',
+                                                   'client_id': self.app_id,
+                                                   'client_secret': self.secret,
+                                                   'code': auth_code})).json()
 
-        access_token = response['access_token']
+        @trace('YandexOAuth.request_data')
+        def request_data(swap_response_json: dict) -> dict:
+            return requests.get(url='https://login.yandex.ru/info?',
+                                params=urlencode({'Authorization': 'OAuth',
+                                                  'oauth_token': swap_response_json['access_token']})).json()
 
-        response = requests.get(
-            'https://login.yandex.ru/info?',
-            urlencode({'Authorization': 'OAuth',
-                       'oauth_token': access_token})).json()
-
+        response = request_data(token_swap())
         return UserService().oauth_authorize(email=response['emails'][0], social_id=str(response['id']),
                                              social_name='YANDEX', useragent=useragent)
+
 
 class GoogleOAuth(OAuthAbstract):
     def __init__(self):
@@ -106,7 +96,9 @@ class GoogleOAuth(OAuthAbstract):
         self.client_secret = self.settings.get('client_secret')
         self.redirect_uri = f'{SETTINGS.BASE_URL}/api/v1/oauth/callback/google'
         self.scope = 'email profile openid'
-        self.authorize_url = f'https://accounts.google.com/o/oauth2/auth?client_id={self.client_id}&access_type=offline&response_type=code&redirect_uri={self.redirect_uri}&scope={self.scope}'
+        self.authorize_url = f'https://accounts.google.com/o/oauth2/auth?client_id={self.client_id}&' \
+                             f'access_type=offline&response_type=code&redirect_uri={self.redirect_uri}&' \
+                             f'scope={self.scope}'
 
     def authorize(self):
         return redirect(self.authorize_url, code=302)
@@ -115,7 +107,7 @@ class GoogleOAuth(OAuthAbstract):
     def callback(self, auth_code, useragent):
         @trace('GoogleOAuth.callback.token_swap')
         def token_swap() -> dict:
-            return requests.post('https://oauth2.googleapis.com/token',
+            return requests.post(url='https://oauth2.googleapis.com/token',
                                  data={'code': auth_code,
                                        'client_id': self.client_id,
                                        'client_secret': self.client_secret,
