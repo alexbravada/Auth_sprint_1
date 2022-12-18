bab42c3745c0205a3d14f8a2ff18f93e4308345d
boris <boris.vladimirov13@gmail.com>
2022-12-19 03:22:22 +0600

    added private API routes

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -2,9 +2,11 @@ from flask import Blueprint
 
 from .auth import auth_dir_bp
 from .oauth import oauth_dir_bp
-
+from .private import private_dir_bp
 
 v1_blueprint = Blueprint('v1', __name__, url_prefix='/v1')
 
 v1_blueprint.register_blueprint(auth_dir_bp)
 v1_blueprint.register_blueprint(oauth_dir_bp)
+v1_blueprint.register_blueprint(private_dir_bp)
+
---
+++ src/api/v1/private/__init__.py
@@ -0,0 +1,6 @@
+from flask import Blueprint
+from .private import private_bp
+
+private_dir_bp = Blueprint('private', __name__, url_prefix='/private')
+
+private_dir_bp.register_blueprint(private_bp)
